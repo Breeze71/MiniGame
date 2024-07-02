9770fb7e8ab72e8dd20193b8bbeab02513d7e236
V <j92080412@gmail.com>
2024-07-02 23:02:29 +0800

    Soldier BTN

--- Assets/_TowerDefense/Script/UI/SoilderBTNUI.cs
+++ Assets/_TowerDefense/Script/UI/SoilderBTNUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,9 +15,31 @@ namespace V.TowerDefense
         [Expandable] public SoilderSO soilderConfig;
         [field : SerializeField] public Button Button {get; private set;}
 
+        [SerializeField] private TextMeshProUGUI _attackTEXT;
+        [SerializeField] private TextMeshProUGUI _healthTEXT;
+
         private void Awake() 
         {
             Button = GetComponentInChildren<Button>();    
+        }
+
+        private void Start() 
+        {
+            UpdateSoldierUI();    
+        }
+
+        public void UpdateSoldierUI()
+        {
+            if(soilderConfig != null)
+            {
+                _attackTEXT.text = soilderConfig.Attack.ToString();
+                _healthTEXT.text = soilderConfig.Health.ToString();
+            }
+            else
+            {
+                _attackTEXT.text = "---";
+                _healthTEXT.text = "---";
+            }
         }
     }
 }
