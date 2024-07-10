51a5c381185732363b5ed579a2f8075a3f77fda6
V <j92080412@gmail.com>
2024-07-10 20:51:54 +0800

    Fix

--- Assets/_TowerDefense/Script/Unit/UnitBase.cs
+++ Assets/_TowerDefense/Script/Unit/UnitBase.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Dynamic;
-using Microsoft.Unity.VisualStudio.Editor;
 using NaughtyAttributes;
 using UnityEngine;
 using Timer = V.Utilities.Timer;
